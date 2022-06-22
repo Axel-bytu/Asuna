c3dd9c61db3879b433326369b64afbeaa0d3a0cc
FadliDarmawan <fadli.darmawan03@gmail.com>
2022-06-22 11:00:16 +0700

    vv

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process'
 import fs from 'fs'
 let handler = async (m, { conn, text, isROwner }) => {
-  if (global.conn.user.jid == conn.user.jid) {
+  if (conn.user.jid == conn.user.jid) {
     let stdout = execSync('git remote set-url origin https://github.com/FadliDarmawan/nutella.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) fs.readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
