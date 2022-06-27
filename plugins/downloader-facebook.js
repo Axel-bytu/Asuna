c476b9c4a41f2bffcf31b1e69be606caa60b3e6e
FadliDarmawan <fadli.darmawan03@gmail.com>
2022-06-27 16:40:00 +0700

    bisa kh

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -2,7 +2,7 @@ import { facebookDl } from './scrape.js'
 import { savefrom } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-	if (!args[0]) throw `Masukkan URL facebook yang ingin didownload. ${usedPrefix + command} https://www.facebook.com/KassBook/videos/3403431609714277/`
+	if (!args[0]) throw `Masukkan URL facebook yang ingin didownload.\n\n${usedPrefix + command} https://www.facebook.com/KassBook/videos/3403431609714277/`
 	let res = await facebookDl(args[0]).catch(async _ => await savefrom(args[0])).catch(_ => null)
 	if (!res) throw 'Can\'t download the post'
 	let url = res?.url?.[0]?.url || res?.url?.[1]?.url || res?.['720p'] || res?.['360p']
