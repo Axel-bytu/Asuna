c2afe43e3aed2412c86394b9f4526d1092be65cf
FadliDarmawan <fadli.darmawan03@gmail.com>
2022-07-03 07:05:28 +0700

    f

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -2,7 +2,7 @@ import { execSync } from 'child_process'
 import fs from 'fs'
 let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/FadliDarmawan/haruno-md.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git remote set-url origin https://github.com/FadliDarmawan/nutella.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) fs.readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
   }
@@ -10,16 +10,4 @@ let handler = async (m, { conn, text, isROwner }) => {
 handler.help = ['update']
 handler.tags = ['host']
 handler.command = /^update$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-
 export default handler
