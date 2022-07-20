e5f0b745a592417a216fe98d058712bcaece0066
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-20 18:09:33 -0500

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { participants, conn }) => {
     let name = await conn.getName(m.chat)
     db.data.chats[m.chat].isBanned = true
-    m.reply(`*${conn.getName(conn.user.jid)}* tidak aktif di chat ${name}`)
+    m.reply(`*${conn.getName(conn.user.jid)}* Bot Asuna no activa en el chat ${name}`)
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
