ad695c3b3435532d439b36fd00129733188902ac
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-20 22:49:22 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,25 +1,11 @@
-import PhoneNumber from 'awesome-phonenumber'
-let handler = async(m, { conn }) => {
-    let nomor = owner[0][0]
-    let number = nomor + '@s.whatsapp.net'
-    let biz = await conn.getBusinessProfile(number)
 
-    let vcard = `
-BEGIN:VCARD
-VERSION:3.0
-N:;Axel;;;
-FN:Axel
-TEL;type=CELL;type=VOICE;waid=${nomor}:${PhoneNumber('+' + nomor).getNumber('international')}
-X-WA-BIZ-NAME:Axel
-X-WA-BIZ-DESCRIPTION:${biz.description.replace(/\n/g, '\\n')}
-END:VCARD
-    `.trim()
-    let kont = await conn.sendMessage(m.chat, { contacts: { displayName: 'Axel', contacts: [{vcard}]}}, { quoted: m})
-    conn.reply(m.chat, kont)
+let handler = function (m) {
+  // this.sendContact(m.chat, '593 96 268 1710', 'Broz', m)
+  this.sendContact(m.chat, '593962681710', 'Broz', m)
 }
 handler.help = ['owner', 'creador']
 handler.tags = ['info']
 
 handler.command = /^(owner|creador)$/i
 
-export default handler
+module.exports = handler
