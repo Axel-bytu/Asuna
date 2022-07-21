67a99e8430ba36ff7a3388c6a17157fd9576c594
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-20 22:05:13 -0500

    Update owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,11 +1,11 @@
 // import db from '../lib/database.js'
 
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'Siapa yang mau di unbanned?'
+    if (!text) throw '¿Quién quiere ser desbaneado?'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag salah satu lah'
+    if (!who) throw 'Etiqueta a un usuario'
     let users = db.data.users
     users[who].banned = false
     conn.reply(m.chat, `*${conn.getName(conn.user.jid)}* di unban dari ${conn.getName(who)}`, m)
