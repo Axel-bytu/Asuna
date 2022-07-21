74916659996e2268c33bac8792af4faeaa5d4089
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-21 18:02:54 -0500

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Harap masukkan URL instagram yang ingin didownload.\n\nContoh: ${usedPrefix + command} https://www.instagram.com/p/CfDVJyOPsqF/?utm_source=ig_web_copy_link`
+    if (!args[0]) throw `Ingrese la URL de Instagram que desea descargar.\n\nEjemplo: ${usedPrefix + command} https://www.instagram.com/p/CfDVJyOPsqF/?utm_source=ig_web_copy_link`
     const results = await instagramdl(args[0])
         .catch(async _ => await instagramdlv2(args[0]))
         .catch(async _ => await instagramdlv3(args[0]))
