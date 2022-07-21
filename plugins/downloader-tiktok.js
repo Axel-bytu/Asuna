de1714c166c3384ae20f9cbf614adf9a806149f0
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-21 18:08:40 -0500

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import { tiktok } from './scrape.js'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `Masukkan URL tiktok yang ingin didownload!\n\nContoh: ${usedPrefix + command} https://vt.tiktok.com/ZSe5pocWX/` 
+	if (!text) throw `¡Ingrese la URL de tiktok que desea descargar!\n\nEjemplo: ${usedPrefix + command} https://vt.tiktok.com/ZSe5pocWX/` 
 	if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw 'Invalid URL!\nPastikan URL merupakan URL tiktok.'
 	let result = await tiktok(text)
 	await conn.sendFile(m.chat, result.nowm, 'tiktok.mp4', watermark, m)
