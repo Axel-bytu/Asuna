f31433c5d9d4b3bd3006a189134f65bf31833601
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-21 17:39:19 -0500

    Update cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -3,9 +3,9 @@
 let handler = async (m, { text }) => {
     let hash = text
     if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-    if (!hash) throw `Hash tidak ditemukan.`
+    if (!hash) throw `Hashes no encontrados.`
     let sticker = db.data.sticker
-    if (sticker[hash] && sticker[hash].locked) throw 'Kamu tidak memiliki izin untuk menghapus perintah stiker ini.'
+    if (sticker[hash] && sticker[hash].locked) throw 'No tienes permiso para eliminar este comando de pegatina.'
     delete sticker[hash]
     m.reply(`Berhasil!`)
 }
