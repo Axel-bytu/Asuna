049136a283d95c370306402bafe1a0ce4c5a2d3a
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-21 18:07:13 -0500

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,6 +1,6 @@
 import { mediafiredl } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Masukkan URL mediafire yang ingin didownload.\n\nContoh: ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
+    if (!args[0]) throw `Ingrese la URL de Mediafire que desea descargar.\n\nEjemplo: ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
     let res = await mediafiredl(args[0])
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
     let caption = `
