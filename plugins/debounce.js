4f8bac7f80d82945a3c0cde35e7c811bf9c3ddd0
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-21 17:53:57 -0500

    Update debounce.js

--- plugins/debounce.js
+++ plugins/debounce.js
@@ -2,10 +2,10 @@ import { spawn } from 'child_process';
 let handler = async (m, { conn }) => {
   if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
   if (global.conn.user.jid == conn.user.jid) {
-    await m.reply('Sedang merestart bot...')
+    await m.reply('Reiniciando bots...')
     await global.db.write()
     process.send('reset')
-  } else throw 'Error. Tempat run tidak mendukung fitur debounce.'
+  } else throw 'Error. El lugar de ejecución no es compatible con la función debounce.'
 }
 handler.help = ['debounce' + (process.send ? '' : ' (Tidak Bekerja)')]
 handler.tags = ['host']
