a1560be9b69a9ee5f515a40415b2014439233835
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-21 18:58:42 -0500

    Update fun-apakah2.js

--- plugins/fun-apakah2.js
+++ plugins/fun-apakah2.js
@@ -1,6 +1,6 @@
 let handler = async (m, { command, text }) => m.reply(`
-*Pertanyaan:* ${command} ${text}
-*Jawaban:* ${['Ya', 'Mungkin iya', 'Mungkin', 'Mungkin tidak', 'Tidak', 'Tidak mungkin'].getRandom()}
+*Pregunta:* ${command} ${text}
+*Respuesta:* ${['Ya', 'Quizás sí', 'Probablemente', 'Quizás no', 'No', 'De ninguna manera'].getRandom()}
   `.trim(), null, m.mentionedJid ? {
   mentions: m.mentionedJid
 } : {})
