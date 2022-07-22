0b30a0b38324a49ac17b8c1e29f746c30fd451de
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-21 19:56:40 -0500

    Update group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -2,13 +2,13 @@ import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, args }) => {
     let group = m.chat
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'Hanya bisa dibuka di grup'
+    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'Solo se puede abrir en grupos'
     let groupMetadata = await conn.groupMetadata(group)
     if (!groupMetadata) throw 'groupMetadata is undefined :\\'
     if (!('participants' in groupMetadata)) throw 'participants is not defined :('
     let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!me) throw 'Bot tidak ada di group tersebut.'
-    if (!me.admin) throw 'Bot bukan admin di group tersebut.'
+    if (!me) throw 'El bot no está en el grupo.'
+    if (!me.admin) throw 'El bot no es administrador en el grupo.'
     m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
 }
 handler.help = ['linkgroup']
