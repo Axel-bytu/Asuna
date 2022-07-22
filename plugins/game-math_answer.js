b1a233cc956e1f8d91926600d928f2dd80d09436
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-21 19:47:24 -0500

    Update game-math_answer.js

--- plugins/game-math_answer.js
+++ plugins/game-math_answer.js
@@ -6,20 +6,20 @@ handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.text || !/^Berapa hasil dari/i.test(m.quoted.text)) return !0
     this.math = this.math ? this.math : {}
-    if (!(id in this.math)) return this.sendButton(m.chat, 'Soal itu telah berakhir', watermark, null, [['math', '/math']], m)
+    if (!(id in this.math)) return this.sendButton(m.chat, 'El asunto ha terminado', watermark, null, [['math', '/math']], m)
     if (m.quoted.id == this.math[id][0].id) {
         let math = JSON.parse(JSON.stringify(this.math[id][1]))
         if (m.text == math.result) {
             db.data.users[m.sender].exp += math.bonus
             clearTimeout(this.math[id][3])
             delete this.math[id]
-            this.sendButton(m.chat, `*Jawaban Benar!*\n+${math.bonus} XP`, watermark, null, [['again', `/math ${math.mode}`]], m)
+            this.sendButton(m.chat, `*¡Respuesta correcta!*\n+${math.bonus} XP`, watermark, null, [['again', `/math ${math.mode}`]], m)
         } else {
             if (--this.math[id][2] == 0) {
                 clearTimeout(this.math[id][3])
                 delete this.math[id]
-                this.sendButton(m.chat, `*Kesempatan habis!*\nJawaban: *${math.result}*`, watermark, null, [['again', `/math ${math.mode}`]], m)
-            } else m.reply(`*Jawaban Salah!*\nMasih ada ${this.math[id][2]} kesempatan`)
+                this.sendButton(m.chat, `*¡Se acaba la oportunidad!*\nResponder: *${math.result}*`, watermark, null, [['again', `/math ${math.mode}`]], m)
+            } else m.reply(`*¡Respuesta incorrecta!*\nTodavía hay ${this.math[id][2]} oportunidad`)
         }
     }
     return !0
