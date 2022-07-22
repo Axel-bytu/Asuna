d67c8d110de8ff19535f25d718aedfcee9c5319c
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-21 19:41:36 -0500

    Update game-math.js

--- plugins/game-math.js
+++ plugins/game-math.js
@@ -3,21 +3,21 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     const buttons = Object.keys(modes).map(v => [v, `${usedPrefix}${command} ${v}`])
     if (args.length < 1) return conn.sendButton(m.chat, `
   Mode: ${Object.keys(modes).join(' | ')}
-  Contoh penggunaan: ${usedPrefix}math medium
+  Ejemplo de uso: ${usedPrefix}math medium
   `.trim(), watermark, null, buttons, m)
     let mode = args[0].toLowerCase()
     if (!(mode in modes)) return conn.sendButton(m.chat, `
   Mode: ${Object.keys(modes).join(' | ')}
-  Contoh penggunaan: ${usedPrefix}math medium
+  Ejemplo de uso: ${usedPrefix}math medium
     `.trim(), watermark, null, buttons, m)
     let id = m.chat
-    if (id in conn.math) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.math[id][0])
+    if (id in conn.math) return conn.reply(m.chat, 'Todavía hay preguntas sin respuesta en este chat', conn.math[id][0])
     let math = genMath(mode)
     conn.math[id] = [
-        await conn.reply(m.chat, `Berapa hasil dari *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nBonus Jawaban Benar: ${math.bonus} XP`, m),
+        await conn.reply(m.chat, `¿Cuál es el resultado de *${math.str}*?\n\nSe acabó el tiempo: ${(math.time / 1000).toFixed(2)} detik\nBono de respuesta correcta: ${math.bonus} XP`, m),
         math, 4,
         setTimeout(() => {
-            if (conn.math[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah ${math.result}`, watermark, null, [['again', `${usedPrefix}${command} ${math.mode}`], ...buttons], conn.math[id][0])
+            if (conn.math[id]) conn.sendButton(m.chat, `¡Se acabó el tiempo!\nLa respuesta es ${math.result}`, watermark, null, [['again', `${usedPrefix}${command} ${math.mode}`], ...buttons], conn.math[id][0])
             delete conn.math[id]
         }, math.time)
     ]
