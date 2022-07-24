83d32c4cd2c2db6512ee8a3c86523bfcd937f63f
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-24 09:35:54 -0500

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -2,7 +2,7 @@ import { lyrics, lyricsv2 } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-    if (!teks) throw `Use example ${usedPrefix}${command} hallo`
+    if (!teks) throw `Ejemplo de uso ${usedPrefix}${command} hallo`
     const result = await lyricsv2(teks).catch(async _ => await lyrics(teks))
     m.reply(`
 *${result.title}*
