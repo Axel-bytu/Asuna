48bf14d3ca273af44333be3091fd4351b42a30ca
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-24 09:08:59 -0500

    Update image-wallpaper.js

--- plugins/image-wallpaper.js
+++ plugins/image-wallpaper.js
@@ -1,6 +1,6 @@
 import { wallpaper, wallpaperv2 } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Masukkan query yang ingin dicari.\n\nContoh: ${ usedPrefix + command } Shinao Hiiragi`
+    if (!text) throw `Ingrese la consulta que desea buscar.\n\nEjemplo: ${ usedPrefix + command } Shinao Hiiragi`
     const res = await (/2/.test(command) ? wallpaperv2 : wallpaper)(text)
     const img = res[Math.floor(Math.random() * res.length)]
     conn.sendFile(m.chat, img, 'wallpaper.jpg', watermark, m)
