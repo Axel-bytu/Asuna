da1616d876f24fe6a563ebe9f6747af8aeed4487
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-24 10:39:14 -0500

    Update sticker-toimg2.js

--- plugins/sticker-toimg2.js
+++ plugins/sticker-toimg2.js
@@ -1,6 +1,6 @@
 import { webp2png } from '../lib/webp2mp4.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `Reply sticker with command *${usedPrefix + command}*`
+    const notStickerMessage = `Etiquete el sticker seguido del comando *${usedPrefix + command}*`
     if (!m.quoted) throw notStickerMessage
     const q = m.quoted || m
     let mime = q.mediaType || ''
