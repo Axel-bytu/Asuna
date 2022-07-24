e010266e777ea1b6a43ec534f9a39406e066615c
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-24 09:53:22 -0500

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,14 +1,14 @@
 // import db from '../lib/database.js'
 
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'Siapa yang mau di banned?'
+    if (!text) throw '¿Quién quiere ser prohibido de usar a Asuna bot?'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag salah satu lah'
+    if (!who) throw 'Etiqueta a un usurio'
     let users = db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `berhasil banned`, m)
+    conn.reply(m.chat, `prohibido con éxito`, m)
 }
 handler.help = ['ban @user']
 handler.tags = ['owner']
