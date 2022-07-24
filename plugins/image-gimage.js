d493930f3b14b24e711e58b51b365540ef15aa22
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-24 09:03:36 -0500

    Update image-gimage.js

--- plugins/image-gimage.js
+++ plugins/image-gimage.js
@@ -1,6 +1,6 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Masukkan query yang ingin dicari!\n\nContoh: ${ usedPrefix + command } Shinao Hiiragi`
+    if (!text) throw `Introduzca la consulta que desea encontrar!\n\nEjemplo: ${ usedPrefix + command } Shinao Hiiragi`
     const res = await googleImage(text)
     conn.sendFile(m.chat, res.getRandom(), 'gimage.jpg', watermark, m)
 }
