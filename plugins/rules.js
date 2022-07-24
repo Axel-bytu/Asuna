bb8266d0e20463e80f4c5fb514f7505b61015612
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-24 10:19:34 -0500

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -1,35 +1,35 @@
 let handler = async(m, { conn }) => {
     let caption = `
-*Kebijakan Privasi, Syarat Ketentuan dan Peraturan Haruno Bot*
+*Asuna Bot Política de Privacidad, Términos y Condiciones y Reglamento*
 
-Kebijakan Privasi
-1. Harunobot tidak akan merekam data riwayat chat user.
-2. Harunobot tidak akan menyebarkan nomor users.
-3. Harunobot tidak akan menyimpan media yang dikirimkan oleh users.
-4. Harunobot tidak akan menyalah gunakan data data users.
-5. Owner Harunobot berhak melihat data riwayat chat users.
-6. Owner Harunobot berhak melihat status users.
-7. Owner Harunobot dapat melihat riwayat chat, dan media yang dikirimkan users.
+Política de privacidad
+1. Asunabot no registrará los datos del historial de chat del usuario.
+2. Asunabot no compartirá números de usuario.
+3. Asunabot no guardará los medios enviados por los usuarios.
+4. Asunabot no hará mal uso de los datos del usuario.
+5. El propietario Asunabot tiene derecho a ver los datos del historial de chat de los usuarios.
+6. El propietario Asunabot tiene derecho a ver el estado de los usuarios.
+7. El propietario Asunabot puede ver el historial de chat y los medios enviados por los usuarios.
 
-Peraturan Haruno Bot
-1. Users dilarang menelpon maupun memvideo call nomor bot.
-2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
-3. Users diharap tidak melakukan spam dalam penggunaan bot.
-4. Users dilarang menambahkan nomor bot secara illegal, untuk menambahkan silahkan hubungi owner.
+Reglas del robot Haruno
+1. Los usuarios tienen prohibido llamar o hacer videollamadas a los números de bot.
+2. Los usuarios tienen prohibido enviar varios errores, virtexes, etc. al número de bot.
+3. Se espera que los usuarios no hagan spam en el uso de bots.
+4. Los usuarios tienen prohibido agregar números de bot ilegalmente, para agregar, comuníquese con el propietario.
 
-Syarat Ketentuan Haruno Bot
-1. Harunobot dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
-2. Harunobot *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
-3. Harunobot akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
-Request fitur, kritik saran, atau laporan bug silahkan chat ke owner.
+Términos y condiciones del robot Haruno
+1. Harunobot puede prohibir a los usuarios unilateralmente, independientemente de si se trata de usuarios incorrectos o no.
+2. Harunobot *no será responsable de nada que los usuarios hagan con la función de bot.*
+3. Harunobot impondrá sanciones: bloqueo o prohibición a los usuarios que violen las reglas.
+Solicitudes de funciones, sugerencias o informes de errores, chatee con el propietario.
 
-Kebijakan Peraturan per: 20 April 2022
+Política regulatoria a partir del: 20 de abril de 2022
 ${watermark}`.trim()
     await conn.reply(m.chat, caption, m, { contextInfo: {
         externalAdReply: {
-            sourceUrl: 'https://youtu.be/JktyyWr1N6I',
+            sourceUrl: 'https://Instagram.com/asunabot',
             title: 'Rules',
-            body: 'Haruno',
+            body: 'Asuna',
             thumbnailUrl: global.image
         }
     }})
