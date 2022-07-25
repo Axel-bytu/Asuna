22c4ebbbc6b0d0730b29c2cb9bccf8781e19a9c6
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-25 07:53:13 -0500

    Update tool-sendquote.js

--- plugins/tool-sendquote.js
+++ plugins/tool-sendquote.js
@@ -1,7 +1,7 @@
 async function handler(m) {
     if (!m.quoted) throw 'reply pesan!'
     let q = await m.getQuotedObj()
-    if (!q.quoted) throw 'pesan yang anda reply tidak mengandung reply!'
+    if (!q.quoted) throw '¡El mensaje al que está respondiendo no contiene una respuesta!'
     await q.quoted.copyNForward(m.chat, true)
 }
 handler.command = /^q$/i
