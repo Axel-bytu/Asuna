d464b387e6fc71bce4c0a2372295405e511d7e18
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-25 08:18:29 -0500

    Update youtube-yts.js

--- plugins/youtube-yts.js
+++ plugins/youtube-yts.js
@@ -1,18 +1,18 @@
 import yts from 'yt-search'
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `Harap masukkan query yang ingin dicari!\n\nContoh: ${usedPrefix + command} video protes tiananmen square 1989`
+  if (!text) throw `¡Por favor ingrese la consulta que desea buscar!\n\nEjemplo: ${usedPrefix + command} video protes tiananmen square 1989`
   let results = await yts(text)
   let teks = results.all.map(v => {
     switch (v.type) {
       case 'video': return `
-Judul: ${v.title}
+Título: ${v.title}
 Link: ${v.url}
-Durasi: ${v.timestamp}
-Diupload: ${v.ago}
-Penonton: ${v.views}
+Duración: ${v.timestamp}
+Subido: ${v.ago}
+Audiencia: ${v.views}
       `.trim()
       case 'channel': return `
-Nama: ${v.name}
+Nombre: ${v.name}
 Link: ${v.url}
 Subscriber: ${v.subCountLabel} (${v.subCount})
 Video: ${v.videoCount}
