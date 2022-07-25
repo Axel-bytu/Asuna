b3f8fd8f5e2377326ad94bcc79c5afaec616fdac
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-25 08:02:27 -0500

    Update tool-upload.js

--- plugins/tool-upload.js
+++ plugins/tool-upload.js
@@ -4,13 +4,13 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m, { usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Reply media dengan perintah *${usedPrefix + command}*`
+  if (!mime) throw `Responder a los medios con comandos *${usedPrefix + command}*`
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   m.reply(`${link}
 ${media.length} Byte(s)
-${isTele ? '(Tidak Ada Tanggal Kedaluwarsa)' : '(Tidak diketahui)'}`)
+${isTele ? '(Sin fecha de vencimiento)' : '(Desconocido)'}`)
 }
 handler.help = ['upload']
 handler.tags = ['tools']
