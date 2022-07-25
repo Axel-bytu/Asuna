96d2ab6d8fe538f5a14999b14f404cc5af10d9e4
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-25 07:51:14 -0500

    Update tool-readviewonce.js

--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -2,7 +2,7 @@ let { downloadContentFromMessage } = (await import('@adiwajshing/baileys'));
 
 let handler = async (m, { conn }) => {
     if (!m.quoted) throw 'where\'s message?'
-    if (m.quoted.mtype !== 'viewOnceMessage') throw 'Itu bukan pesan viewOnce'
+    if (m.quoted.mtype !== 'viewOnceMessage') throw 'Eso no es un mensaje viewOnce'
     const buffer = await m.quoted.download()
     const media = m.quoted.mediaMessage[m.quoted.mediaType]
     conn.sendFile(m.chat, buffer, /video/.test(media.mimetype) ? 'video.mp4' : 'image.jpg', media.caption + `\n\n${watermark}`|| watermark, m)
