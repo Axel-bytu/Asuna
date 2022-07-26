e46169efcce43743cef0889c1144253612b22a28
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-26 11:15:29 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,28 +1,29 @@
-import PhoneNumber from 'awesome-phonenumber'
-let handler = async(m, { conn }) => {
-    let nomor = owner[0][0]
-    let number = nomor + '@s.whatsapp.net'
-    let biz = await conn.getBusinessProfile(number)
+import fs from 'fs'
+function handler(m, { conn }) {
+let text = `
+*𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤* 
+*Wa.me/593962681710 (CREADOR)*
+*https://instagram.com/asunabot*
+`.trim()   
+conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝘼𝙨𝙪𝙣𝙖𝘽𝙤𝙩 | 𝘼𝙨𝙪𝙣𝙖 𝙔 𝙆𝙞𝙧𝙞𝙩𝙤',
+body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫𝐚 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',         
+previewType: 0, thumbnail: fs.readFileSync("./media/Asuna.jpg"),
+sourceUrl: `https://wa.me/593962681710`}}})
+  
+//const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+//this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+  
+let pp = './media/menus/Menu2.jpg'
+let str = `🧡 *Eso son los contactos para ti.*`
 
-    let vcard = `
-BEGIN:VCARD
-VERSION:3.0
-N:;Axel;;;
-FN:Axel
-TEL;type=CELL;type=VOICE;waid=${nomor}:${PhoneNumber('+' + nomor).getNumber('international')}
-X-WA-BIZ-NAME:Axel
-X-WA-BIZ-DESCRIPTION:${biz.description.replace(/\n/g, '\\n')}
-END:VCARD
-    `.trim()
-
-    let kont = await conn.sendMessage(m.chat, { contacts: { displayName: 'Axel', contacts: [{vcard}]}}, { quoted: m})
-    let kont2 = await conn.sendContact(m.chat, [[`${owner[1][0]}`, 'Zaki'], [`${owner[2][0]}`, 'Rafli']], m)
-    conn.reply(m.chat, 'Nomor owner itu bukan bot, tidak usah chat command bot.\nChat yang sopan kalau belum dibales ya jangan spam. Makasih', kont)
-    conn.reply(m.chat, 'Nomor partner owner', kont2)
+conn.sendHydrated(m.chat, str, wm, pp, 'https://instagram.com/asunabot', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
+['🐈 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 ', '.infobot'],
+['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 ', '/menu']
+], m,)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
-
-handler.command = /^(owner|creator)$/i
-
+handler.command = /^(contacto|owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
 export default handler
