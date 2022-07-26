9b4140cf02c1d9288e7c771b034ac3f26724cb7f
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-26 15:22:37 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -12,16 +12,6 @@ body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫𝐚 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',
 previewType: 0, thumbnail: fs.readFileSync("./media/Asuna.jpg"),
 sourceUrl: `https://wa.me/593962681710`}}})
   
-//const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-//this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
-  
-let pp = './media/Asuna.jpg'
-let str = `🧡 *Eso son los contactos para ti.*`
-
-conn.sendHydrated(m.chat, str, pp, 'https://instagram.com/asunabot', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['🐈 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 ', '.infobot'],
-['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 ', '/menu']
-], m,)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
