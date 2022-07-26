4a1f131c2e5e686a3cfb87917697c42d778c4468
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-26 09:04:54 -0500

    Create sn.js

---
+++ plugins/sn.js
@@ -0,0 +1,17 @@
+const { createHash } = require('crypto')
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { text, usedPrefix }) {
+let user = global.DATABASE._data.users[m.sender]
+let sn = createHash('md5').update(m.sender).digest('hex')
+    m.reply(`
+  Su número de serie :\n
+  sn : ${sn}
+  `.trim())
+}
+handler.help = ['serial']
+handler.tags = ['info']
+handler.register = true
+
+handler.command = /^serial$/i
+
+module.exports = handler
