1ce6864a082eb4b6828256efd437e7615dab14f5
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-28 18:48:53 -0500

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -5,9 +5,7 @@ let res = await fetch(`https://api-sv2.simsimi.net/v2/?text=${text}&lc=es`)
 let json = await res.json()
 let tes = json.success.replace('simsimi', 'simsimi').replace('Simsimi', 'Simsimi').replace('sim simi', 'sim simi')
 //m.reply(`${tes}`) 
-conn.sendHydrated(m.chat, `${tes}`, `𝘼𝙄 | ${wm}`, null, null, null, null, null, [
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́', '/menu']
-], m)
+conn.sendHydrated(m.chat, `${tes}`, m)
 }
 handler.help = ['asuna']
 handler.tags = ['fun']
