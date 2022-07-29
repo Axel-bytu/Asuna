83074b486dbde7d391fe6b5c45f73bf38e113798
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-29 12:18:43 -0500

    Update group-config.js

--- plugins/group-config.js
+++ plugins/group-config.js
@@ -27,7 +27,7 @@ conn.sendButton(m.chat, `𝙎𝙊𝙇𝙊 𝙇𝙊𝙎 𝘼𝘿𝙈𝙄𝙉𝙎 
 }  
  }
 handler.help = ['group open / close', 'grupo abrir / cerrar']
-handler.tags = ['group']
+handler.tags = ['admin']
 handler.command = /^(group|grupo)$/i
 handler.admin = true
 handler.botAdmin = true
