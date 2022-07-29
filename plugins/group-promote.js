928e2b3b79f2ae989a69f07b1989a60aecec625e
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-29 12:17:08 -0500

    Update group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -7,8 +7,8 @@ var number = text.split`@`[1]
 var number = text
 }
 	
-if(!text && !m.quoted) return conn.reply(m.chat, `${mg}𝘿𝙀𝘽𝙀 𝘿𝙀 𝙐𝙎𝘼𝙍 𝘿𝙀 𝙇𝘼 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 𝙈𝘼𝙉𝙀𝙍𝘼:\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix}darpoder @tag*\n*${usedPrefix}daradmin responder a un mensaje*\n\n𝙔𝙊𝙐 𝙈𝙐𝙎𝙏 𝙐𝙎𝙀 𝘼𝙎 𝙁𝙊𝙇𝙇𝙊𝙒𝙎:\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix}promote @tag*\n*${usedPrefix}promote reply to a message*`, m)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `${fg}𝙀𝙇 𝙉𝙐𝙈𝙀𝙍𝙊 𝙀𝙎 𝙄𝙉𝘾𝙊𝙍𝙍𝙀𝘾𝙏𝙊, 𝙄𝙉𝙏𝙀𝙉𝙏𝙀 𝘿𝙀 𝙉𝙐𝙀𝙑𝙊\n\n𝙏𝙃𝙀 𝙉𝙐𝙈𝘽𝙀𝙍 𝙄𝙎 𝙒𝙍𝙊𝙉𝙂, 𝙏𝙍𝙔 𝘼𝙂𝘼𝙄𝙉`, m)
+if(!text && !m.quoted) return conn.reply(m.chat, `𝘿𝙀𝘽𝙀 𝘿𝙀 𝙐𝙎𝘼𝙍 𝘿𝙀 𝙇𝘼 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 𝙈𝘼𝙉𝙀𝙍𝘼:\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix}darpoder @tag*\n*${usedPrefix}daradmin responder a un mensaje*`, m)
+if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `𝙀𝙇 𝙉𝙐𝙈𝙀𝙍𝙊 𝙀𝙎 𝙄𝙉𝘾𝙊𝙍𝙍𝙀𝘾𝙏𝙊, 𝙄𝙉𝙏𝙀𝙉𝙏𝙀 𝘿𝙀 𝙉𝙐𝙀𝙑𝙊`, m)
 	
 try {
 if(text) {
@@ -23,7 +23,7 @@ var user = number + '@s.whatsapp.net'
 conn.groupParticipantsUpdate(m.chat, [user], 'promote')
 //conn.reply(m.chat, `𝘼𝙃𝙊𝙍𝘼 𝙏𝙄𝙀𝙉𝙀 𝙀𝙇 𝙋𝙊𝘿𝙀𝙍 𝘿𝙀𝙇 𝘼𝘿𝙈𝙄𝙉 😼`, m)
 	
-conn.sendHydrated(m.chat, `${eg}𝘼𝙃𝙊𝙍𝘼 𝙏𝙄𝙀𝙉𝙀 𝙀𝙇 𝙋𝙊𝘿𝙀𝙍 𝘿𝙀𝙇 𝘼𝘿𝙈𝙄𝙉 😼`, watermark, null, null, null, [
+conn.sendHydrated(m.chat, `𝘼𝙃𝙊𝙍𝘼 𝙏𝙄𝙀𝙉𝙀 𝙀𝙇 𝙋𝙊𝘿𝙀𝙍 𝘿𝙀𝙇 𝘼𝘿𝙈𝙄𝙉 😼`, watermark, null, null, null, [
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu'] 
 ], m)
 }}
