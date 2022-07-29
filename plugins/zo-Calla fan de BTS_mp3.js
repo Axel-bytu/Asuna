0b31b0c5249061523e35587ca516d9bc978c8d4b
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-29 17:57:26 -0500

    Update and rename so-Calla fan de BTS_mp3.js to zo-Calla fan de BTS_mp3.js

--- plugins/zo-Calla fan de BTS_mp3.js
+++ plugins/zo-Calla fan de BTS_mp3.js
@@ -3,8 +3,8 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-global.db.data.users[m.sender].money += 100 
-global.db.data.users[m.sender].exp += 100
+db.data.users[m.sender].money += 100 
+db.data.users[m.sender].exp += 100
   
 let vn = './media/Calla Fan de BTS.mp3'
 conn.sendFile(m.chat, vn, 'Calla Fan de BTS.mp3', null, m, true, {
