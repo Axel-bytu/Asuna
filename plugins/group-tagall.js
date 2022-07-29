572320e38295d42accdf3823ff2ce138b3f4e61b
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-29 12:57:35 -0500

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
 }
 
 handler.help = ['tagall']
-handler.tags = ['group']
+handler.tags = ['group','admin']
 handler.command = ['tagall']
 handler.admin = true
 handler.group = true
