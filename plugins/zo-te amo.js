6d99b632047efed0ca8d2407075b442540333793
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-30 12:08:41 -0500

    Create zo-te amo.js

---
+++ plugins/zo-te amo.js
@@ -0,0 +1,17 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
+db.data.users[m.sender].exp += 10
+  
+let vn = './media/Te-amo.mp3'
+conn.sendFile(m.chat, vn, 'Te-amo.mp3', null, m, true, {
+type: 'audioMessage',
+ptt: true 
+})
+}
+handler.customPrefix = /te amo|teamo/i
+handler.command = new RegExp
+handler.fail = null
+export default handler
