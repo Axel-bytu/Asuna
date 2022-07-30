b6b2debde873f3b2edf1e7e85208ada75363601f
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-30 11:54:18 -0500

    Create zo-niconico_mp3.js

---
+++ plugins/zo-niconico_mp3.js
@@ -0,0 +1,16 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+db.data.users[m.sender].exp += 30
+  
+let vn = './media/niconico.mp3'
+conn.sendFile(m.chat, vn, 'niconico.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /niconico|NICONICO|Niconico|niconiconi|Niconiconi|NICONICONI/ 
+handler.command = new RegExp
+export default handler
