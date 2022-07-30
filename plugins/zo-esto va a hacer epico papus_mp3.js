589910b13b76527f623ff3f351c36261143fa5ba
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-30 11:37:27 -0500

    Create zo-esto va a hacer epico papus_mp3.js

---
+++ plugins/zo-esto va a hacer epico papus_mp3.js
@@ -0,0 +1,16 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
+db.data.users[m.sender].exp += 10
+  
+let vn = './media/esto va a hacer epico papus.mp3'
+conn.sendFile(m.chat, vn, 'esto va a hacer epico papus.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Esto va ser épico papus|esto va ser épico papus|Esto va ser|Esto va a hacer|esto va acer|Esto va aser|esto va ser|esto va a hacer/i 
+handler.command = new RegExp
+export default handler
