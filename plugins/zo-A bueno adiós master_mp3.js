c3bd24ba9d16bb57e0ca840fc5ee1dc85d5d1f26
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-30 09:08:48 -0500

    Update zo-A bueno adiós master_mp3.js

--- plugins/zo-A bueno adiós master_mp3.js
+++ plugins/zo-A bueno adiós master_mp3.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-db.data.users[m.sender].exp += 100
+db.data.users[m.sender].exp += 10
   
 let vn = './media/A bueno adios master.mp3'
 conn.sendFile(m.chat, vn, 'A bueno adios master.mp3', null, m, true, {
