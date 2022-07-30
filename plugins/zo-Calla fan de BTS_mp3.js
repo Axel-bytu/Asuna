8e9a0b31a588b5e289adda1202f95151fb2abf86
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-30 08:28:20 -0500

    Update zo-Calla fan de BTS_mp3.js

--- plugins/zo-Calla fan de BTS_mp3.js
+++ plugins/zo-Calla fan de BTS_mp3.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-global.db.data.users[m.sender].exp += 100
+db.data.users[m.sender].exp += 10
   
 let vn = './media/Calla Fan de BTS.mp3'
 conn.sendFile(m.chat, vn, 'Calla Fan de BTS.mp3', null, m, true, {
