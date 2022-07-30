dafbee5ca3ed14fdd92f925545a3c82988785db0
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-30 08:27:48 -0500

    Update zo-Buen día grupo_mp3.js

--- plugins/zo-Buen día grupo_mp3.js
+++ plugins/zo-Buen día grupo_mp3.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
-global.db.data.users[m.sender].exp += 100
+db.data.users[m.sender].exp += 100
   
 let vn = './media/Buen día grupo.mp3'
 conn.sendFile(m.chat, vn, 'Buen día grupo.mp3', null, m, true, {
