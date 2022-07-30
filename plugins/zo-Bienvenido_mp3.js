913ba625a851dcbe487f87a4306d5c81dd61f5f5
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-30 09:08:24 -0500

    Update zo-Bienvenido_mp3.js

--- plugins/zo-Bienvenido_mp3.js
+++ plugins/zo-Bienvenido_mp3.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
-db.data.users[m.sender].exp += 100
+db.data.users[m.sender].exp += 10
   
 let vn = './media/Bienvenido.mp3'
 conn.sendFile(m.chat, vn, 'Bienvenido.mp3', null, m, true, {
