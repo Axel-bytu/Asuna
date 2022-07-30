766225f22c77a34003bd419ad1325d151985def1
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-30 12:14:05 -0500

    Create zo-verdad-que-te-engañe_mp3.js

---
+++ plugins/zo-verdad-que-te-engañe_mp3.js
@@ -0,0 +1,16 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
+db.data.users[m.sender].exp += 10
+  
+let vn = './media/verdad-que-te-engane.mp3'
+conn.sendFile(m.chat, vn, 'verdad-que-te-engane.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Verdad que te engañe|verdad que te engañe|verdad que|Verdad que/i 
+handler.command = new RegExp
+export default handler
