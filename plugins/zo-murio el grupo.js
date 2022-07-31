4c382978d4140b7ddaab2edb45d1973a94e54f64
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:41:56 -0500

    Update zo-murio el grupo.js

--- plugins/zo-murio el grupo.js
+++ plugins/zo-murio el grupo.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-db.data.users[m.sender].exp += 75
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/Murio.m4a'
 conn.sendFile(m.chat, vn, 'Murio.m4a', null, m, true, {
@@ -13,4 +12,5 @@ ptt: true
 }
 handler.customPrefix = /Murió el grupo|Murio el grupo|murio el grupo|murió el grupo|Grupo muerto|grupo muerto/
 handler.command = new RegExp
+handler.group = true
 export default handler
