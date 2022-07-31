4e9b0f419b3fe8fdad58955a9e06b1d67be479b9
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:36:52 -0500

    Update zo-diagnosticadocongey_mp3.js

--- plugins/zo-diagnosticadocongey_mp3.js
+++ plugins/zo-diagnosticadocongey_mp3.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-db.data.users[m.sender].exp += 10
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/DiagnosticadoConGay.mp3'
 conn.sendFile(m.chat, vn, 'DiagnosticadoConGay.mp3', null, m, true, {
@@ -14,4 +13,5 @@ ptt: true
 handler.customPrefix = /giagnosticadocongay|diagnosticado con gay|diagnosticado gay|te diagnóstico con gay|diagnóstico gay|te diagnostico con gay|te diagnóstico con gay|te diagnosticó con gay|te diagnostico con gay/i
 handler.command = new RegExp
 handler.fail = null
+handler.group = true
 export default handler
