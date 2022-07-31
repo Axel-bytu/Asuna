6276e6d4aa69a9e1eee496bc2fd1a4391ce3024e
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:44:40 -0500

    Update zo-pasa pack_mp3.js

--- plugins/zo-pasa pack_mp3.js
+++ plugins/zo-pasa pack_mp3.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
-db.data.users[m.sender].exp += 10
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/Elmo.mp3'
 conn.sendFile(m.chat, vn, 'Elmo.mp3', null, m, true, {
@@ -13,4 +12,5 @@ ptt: true
 }
 handler.customPrefix = /Pasa pack|vendes tu nudes|pasa video hot|pasa tu pack|pasa fotos hot|vendes tu pack|Vendes tu pack|Vendes tu pack?|vendes tu pack|Pasa Pack Bot|pasa pack Bot|pasa tu pack Bot|Pásame tus fotos desnudas|pásame tu pack|me pasas tu pak|me pasas tu pack|pasa pack/
 handler.command = new RegExp
+handler.group = true
 export default handler
