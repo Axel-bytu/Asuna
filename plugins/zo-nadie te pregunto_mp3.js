740596e1bf776ad86c0490ca80698a2a5dffcaa8
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:42:33 -0500

    Update zo-nadie te pregunto_mp3.js

--- plugins/zo-nadie te pregunto_mp3.js
+++ plugins/zo-nadie te pregunto_mp3.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
-db.data.users[m.sender].exp += 100
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/nadie te pregunto.mp3'
 conn.sendFile(m.chat, vn, 'nadie te pregunto.mp3', null, m, true, {
@@ -13,4 +12,5 @@ ptt: true
 }
 handler.customPrefix = /nadie te pregunto|Nadie te pregunto|Nadie te preguntó|nadie te preguntó/ 
 handler.command = new RegExp
+handler.group = true
 export default handler
