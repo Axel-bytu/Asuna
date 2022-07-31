65224a0515484a5c4cd3a46e6513fd38222fb843
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:12:17 -0500

    Update zo-Me anda buscando anonymous_mp3.js

--- plugins/zo-Me anda buscando anonymous_mp3.js
+++ plugins/zo-Me anda buscando anonymous_mp3.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-db.data.users[m.sender].exp += 10
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/Me anda buscando anonymous.mp3'
 conn.sendFile(m.chat, vn, 'Me anda buscando anonymous.mp3', null, m, true, {
@@ -13,4 +12,5 @@ ptt: true
 }
 handler.customPrefix = /Me anda buscando anonymous|me anda buscando anonymous|Me está buscando anonymous|me está buscando anonymous|Me está buscando anonimo|Me esta buscando anonimo|anonimus|anónimo/i
 handler.command = new RegExp
+handler.group = true
 export default handler
