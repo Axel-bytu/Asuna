a8c944c848d0d34b586001491238c54251bd1901
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:11:02 -0500

    Update zo-En caso de una investigación_mp3.js

--- plugins/zo-En caso de una investigación_mp3.js
+++ plugins/zo-En caso de una investigación_mp3.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-db.data.users[m.sender].exp += 10
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/En caso de una investigación.mp3'
 conn.sendFile(m.chat, vn, 'En caso de una investigación.mp3', null, m, true, {
@@ -13,4 +12,5 @@ ptt: true
 }
 handler.customPrefix = /En caso de una investigación|En caso de una investigacion/i 
 handler.command = new RegExp
+handler.group = true
 export default handler
