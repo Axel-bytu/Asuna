8ba80f437df76afa0d3df91f1d933e4fba254445
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:16:37 -0500

    Update zo-Muchachos_mp3.js

--- plugins/zo-Muchachos_mp3.js
+++ plugins/zo-Muchachos_mp3.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
-db.data.users[m.sender].exp += 10
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/Muchachos.mp3'
 conn.sendFile(m.chat, vn, 'Muchachos.mp3', null, m, true, {
@@ -13,4 +12,5 @@ ptt: true
 }
 handler.customPrefix = /Muchachos/i 
 handler.command = new RegExp
+handler.group = true
 export default handler
