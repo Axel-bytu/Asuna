fafe1500e1f69104774349c33c99a93724f24290
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:50:55 -0500

    Update zo-te amo.js

--- plugins/zo-te amo.js
+++ plugins/zo-te amo.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
-db.data.users[m.sender].exp += 10
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/Te-amo.mp3'
 conn.sendFile(m.chat, vn, 'Te-amo.mp3', null, m, true, {
@@ -14,4 +13,5 @@ ptt: true
 handler.customPrefix = /te amo|teamo/i
 handler.command = new RegExp
 handler.fail = null
+handler.group = true
 export default handler
