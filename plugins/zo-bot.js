8e43c07a5831b97ae35d25a95da8c1abb82d9147
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 06:58:21 -0500

    Create zo-bot.js

---
+++ plugins/zo-bot.js
@@ -0,0 +1,16 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
+  
+let vn = './media/bot.mp3'
+conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /bot|bonito|bosita/i 
+handler.command = new RegExp
+handler.group = true
+export default handler
