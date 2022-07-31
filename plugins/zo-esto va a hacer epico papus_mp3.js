60c4831bf7079ade7ccc2bc27334134856013a75
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:39:17 -0500

    Update zo-esto va a hacer epico papus_mp3.js

--- plugins/zo-esto va a hacer epico papus_mp3.js
+++ plugins/zo-esto va a hacer epico papus_mp3.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
-db.data.users[m.sender].exp += 10
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/esto va a hacer epico papus.mp3'
 conn.sendFile(m.chat, vn, 'esto va a hacer epico papus.mp3', null, m, true, {
@@ -13,4 +12,5 @@ ptt: true
 }
 handler.customPrefix = /Esto va ser épico papus|esto va ser épico papus|Esto va ser|Esto va a hacer|esto va acer|Esto va aser|esto va ser|esto va a hacer/i 
 handler.command = new RegExp
+handler.group = true
 export default handler
