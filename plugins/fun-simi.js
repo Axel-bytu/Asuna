11a881bdbd0da7dad6efa0d874f769fdcad3572b
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-30 22:06:56 -0500

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,11 +1,10 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙃𝘼𝘽𝙇𝘼𝙍 𝘾𝙊𝙉𝙈𝙄𝙂𝙊\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Hola Asuna Bot*`
-let res = await fetch(`https://api-sv2.simsimi.net/v2/?text=${text}&lc=es`)
+let res = await fetch(global.API('https://api-sv2.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
 let json = await res.json()
-let tes = json.success.replace('simsimi', 'simsimi').replace('Simsimi', 'Simsimi').replace('sim simi', 'sim simi')
-//m.reply(`${tes}`) 
-conn.sendHydrated(m.chat, `${tes}`, m)
+if (json.success) m.reply(json.success)
+  else throw json
 }
 handler.help = ['asuna']
 handler.tags = ['fun']
