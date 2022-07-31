97ef060008fa701e606185359e7321a8a817e4a7
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:38:38 -0500

    Update zo-es putoo_mp3.js

--- plugins/zo-es putoo_mp3.js
+++ plugins/zo-es putoo_mp3.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
-db.data.users[m.sender].exp += 10
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/Es putoo.mp3'
 conn.sendFile(m.chat, vn, 'Es putoo.mp3', null, m, true, {
@@ -14,4 +13,5 @@ ptt: true
 handler.customPrefix = /es puto|eeesss putoo|es putoo|esputoo/i
 handler.command = new RegExp
 handler.fail = null
+handler.group = true
 export default handler
