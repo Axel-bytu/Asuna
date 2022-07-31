9e56eab92d92dd7665ddb06ea1c1b1e381f47c74
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-07-31 07:45:55 -0500

    Update zo-quien es tu sempai_mp3.js

--- plugins/zo-quien es tu sempai_mp3.js
+++ plugins/zo-quien es tu sempai_mp3.js
@@ -2,8 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
-db.data.users[m.sender].exp += 10
+if (!global.db.data.settings[conn.user.jid].restrict) throw `𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊`
   
 let vn = './media/Tu.mp3'
 conn.sendFile(m.chat, vn, 'Tu.mp3', null, m, true, {
@@ -13,4 +12,5 @@ ptt: true
 }
 handler.customPrefix = /Quién es tu senpai botsito 7u7|Quien es tu senpai botsito 7u7|Quién es tu sempai botsito 7u7|Quien es tu sempai botsito 7u7|Quién es tu senpai botsito 7w7|Quien es tu senpai botsito 7w7|quién es tu senpai botsito 7u7|quien es tu senpai botsito 7u7|Quién es tu sempai botsito 7w7|Quien es tu sempai botsito 7w7|Quién es tu senpai botsito|Quien es tu senpai botsito|Quién es tu sempai botsito|Quien es tu sempai botsito|Quién es tu senpai botsito|Quien es tu senpai botsito|quién es tu senpai botsito|quien es tu senpai botsito|Quién es tu sempai botsito|Quien es tu sempai botsito/
 handler.command = new RegExp
+handler.group = true
 export default handler
