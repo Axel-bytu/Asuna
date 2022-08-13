6cba4dd55fe7fa73fde556e1970124b6b3720ea3
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-08-13 10:35:57 -0500

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -11,19 +11,19 @@ Política de privacidad
 6. El propietario Asunabot tiene derecho a ver el estado de los usuarios.
 7. El propietario Asunabot puede ver el historial de chat y los medios enviados por los usuarios.
 
-Reglas del robot Haruno
+Reglas del robot Asunabot
 1. Los usuarios tienen prohibido llamar o hacer videollamadas a los números de bot.
 2. Los usuarios tienen prohibido enviar varios errores, virtexes, etc. al número de bot.
 3. Se espera que los usuarios no hagan spam en el uso de bots.
 4. Los usuarios tienen prohibido agregar números de bot ilegalmente, para agregar, comuníquese con el propietario.
 
-Términos y condiciones del robot Haruno
-1. Harunobot puede prohibir a los usuarios unilateralmente, independientemente de si se trata de usuarios incorrectos o no.
-2. Harunobot *no será responsable de nada que los usuarios hagan con la función de bot.*
-3. Harunobot impondrá sanciones: bloqueo o prohibición a los usuarios que violen las reglas.
+Términos y condiciones del robot Asuna
+1. Asunabot puede prohibir a los usuarios unilateralmente, independientemente de si se trata de usuarios incorrectos o no.
+2. Asunabot *no será responsable de nada que los usuarios hagan con la función de bot.*
+3. Asunabot impondrá sanciones: bloqueo o prohibición a los usuarios que violen las reglas.
 Solicitudes de funciones, sugerencias o informes de errores, chatee con el propietario.
 
-Política regulatoria a partir del: 20 de abril de 2022
+Política regulatoria a partir del: 05 de agosto de 2022
 ${watermark}`.trim()
     await conn.reply(m.chat, caption, m, { contextInfo: {
         externalAdReply: {
