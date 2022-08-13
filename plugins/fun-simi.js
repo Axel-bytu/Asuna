ffc6e824768da4cd97e944218a9ca25c7a8e5f35
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-08-13 08:03:58 -0500

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+import fetch = from 'node-fetch' 
 let handler = m => m
 
 handler.before = async (m) => {
@@ -6,7 +6,7 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned && !m.isGroup) {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(global.API('rey', '/api/fun/simsimi.net/v2/?', { text: encodeURIComponent(m.text) }, 'apikey'))
+        let res = await fetch(global.API('rey', '/api/fun/simisimi-ind2?', { text: encodeURIComponent(m.text) }, 'apikey'))
         if (!res.ok) return m.reply(eror)
         let json = await res.json()
         if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
@@ -15,7 +15,4 @@ handler.before = async (m) => {
     }
     return true
 }
-handler.help = ['asuna']
-handler.tags = ['fun']
-handler.command = ['asuna', 'bot', 'simi', 'simsimi', 'alexa', 'bixby', 'cortana', 'siri', 'okgoogle']
-export default handler
+module.exports = handler
