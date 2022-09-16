e3cf075c101b28d909260c08aa60d7f388c44246
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2022-09-16 15:47:41 -0500

    Create vs.js

---
+++ plugins/vs.js
@@ -0,0 +1,33 @@
+let handler = m => m.reply('*⚔️ Clan (x)  vs S4nGrientos ⚔️*\n
+Hora del vs ⌚: (x)\n
+Color de bestimenta: Roja\n 
+🩸𝔼𝕤𝕔𝕦𝕒𝕕𝕣𝕒 #🥇\n
+➪.\n
+➪.\n
+➪.\n
+➪.\n
+🩸𝔼𝕤𝕔𝕦𝕒𝕕𝕣𝕒 #🥈\n
+➪.\n
+➪.\n
+➪.\n
+➪.\n
+🩸𝔼𝕤𝕔𝕦𝕒𝕕𝕣𝕒 #🥉\n
+➪.\n
+➪.\n
+➪.\n
+➪.\n
+🩸𝕊𝕦𝕡𝕝𝕖𝕟𝕥𝕖𝕤 #🏅\n
+➪.\n
+➪.\n
+➪.\n
+➪.\n
+*nota*: tener discord del clan y ser puntual a la hora indicada, leer las reglas del vs.
+')
+
+handler.customPrefix = /^(vs)$/i
+handler.command = new RegExp
+handler.private = false
+handler.group = true
+handler.owner = true
+
+export default handler
